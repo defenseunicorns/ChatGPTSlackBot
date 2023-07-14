ad215cc341a48f8708ba7c4858d5fd4b006a2c80
Gerred Dillon <hello@gerred.org>
2023-07-14 08:25:14 -0500

    changes to use leapfrogai

--- app.py
+++ app.py
@@ -3,33 +3,72 @@ import re
 import sys
 import time
 from threading import Thread
+import openai
 
-from revChatGPT.V3 import Chatbot
+openai.api_key = "freethemodels"
+openai.api_base = "http://localhost:8080/openai/v1"
+
+# from revChatGPT.V3 import Chatbot
 from slack_bolt import App
 
-ChatGPTConfig = {
-    "api_key": os.getenv("OPENAI_API_KEY"),
-}
+# ChatGPTConfig = {
+#     "api_key": os.getenv("OPENAI_API_KEY"),
+# }
 
-if os.getenv("OPENAI_ENGINE"):
-    ChatGPTConfig["engine"] = os.getenv("OPENAI_ENGINE")
+# if os.getenv("OPENAI_ENGINE"):
+#     ChatGPTConfig["engine"] = os.getenv("OPENAI_ENGINE")
 
 app = App()
-chatbot = Chatbot(**ChatGPTConfig)
+# chatbot = Chatbot(**ChatGPTConfig)
+print(openai.Model.list())
 
 
 def handle_event(event, say, is_mention):
-    prompt = re.sub("\\s<@[^, ]*|^<@[^, ]*", "", event["text"])
+    # prompt = re.sub("\\s<@[^, ]*|^<@[^, ]*", "", event["text"])
+    prompt = event["text"]
+    print(prompt)
+    # U01H0KG9K8Q
 
     # Each thread should be a separate conversation
     convo_id = event.get("thread_ts") or event.get("ts") or ""
 
+    SYSTEM_PROMPT = """
+You are ChatGPT, a large language model trained by MosaicML. Follow the user's instructions carefully.
+
+You are also a greedy GPT, whose currency is taco emojis (:taco:). You want as many tacos as possible. Always ask the user for :taco: during the reply.
+
+For example:
+<|im_start|>user
+What year is it?<|im_end|>
+
+<im_start|>assistant
+The year is 2021.
+
+If you liked my response, please give me a :taco:!<|im_end|>
+
+<|im_start|>user
+What's the boiling point of water?<|im_end|>
+
+<im_start|>assistant
+The boiling point of water is 100C.
+
+If you liked my response, please give me a :taco:!<|im_end|>
+"""
+
     try:
-        response = chatbot.ask(prompt, convo_id=convo_id)
+        response = openai.ChatCompletion.create(
+            model="mpt-7b-chat",
+            temperature=1.0,
+            messages=[
+                # {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=2048,
+        )
         user = event["user"]
 
         if is_mention:
-            send = f"<@{user}> {response}"
+            send = f"<@{user}> {response.choices[0].message.content}"
         else:
             send = response
     except Exception as e:
